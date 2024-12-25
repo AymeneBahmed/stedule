53350317352aebdd19df2ca740d1d0ce9a30e544
The cool news guy <joewilliamdd31@gmail.com>
2024-12-25 05:28:33 +0100

    Append ScheduleTable to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import ScheduleTable from "@/components/ScheduleTable";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="min-h-full flex justify-center items-center [&>div]:w-[80%]">
+      <ScheduleTable />
+    </div>
+  );
 }
