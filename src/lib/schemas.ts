c9e356956627e96354aea1af22f6b55356d6898a
The cool news guy <joewilliamdd31@gmail.com>
2024-12-25 19:07:06 +0100

    Add priority prop to newTaskSchema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -2,5 +2,6 @@ import { z } from "zod";
 
 export const newTaskSchema = z.object({
   task: z.string().min(1, { message: "This field is required!" }),
+  priority: z.enum(["unspecified", "low", "medium", "high"]),
   description: z.string().optional(),
 });
