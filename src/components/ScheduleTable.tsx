3d591ec067eaf122204b2e0c17fe2c1bf83d5d97
The cool news guy <joewilliamdd31@gmail.com>
2024-12-25 05:28:13 +0100

    Create ScheduleTable comp with days header

---
+++ src/components/ScheduleTable.tsx
@@ -0,0 +1,21 @@
+import { days } from "@/lib/constants";
+import { Table, TableHead, TableHeader, TableRow } from "./ui/table";
+
+export default function ScheduleTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {/* Additional empty cell */}
+          <TableHead></TableHead>
+
+          {days.map((day) => (
+            <TableHead key={day} className="text-black">
+              {day.toUpperCase()}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+    </Table>
+  );
+}
