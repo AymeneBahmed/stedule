5f65d7ba46ad19ae7f99b7898376d00813269823
The cool news guy <joewilliamdd31@gmail.com>
2024-12-25 19:17:42 +0100

    Remove unnecessary optional constraint for description in newTaskSchema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const newTaskSchema = z.object({
   task: z.string().min(1, { message: "This field is required!" }),
   priority: z.enum(["unspecified", "low", "medium", "high"]),
-  description: z.string().optional(),
+  description: z.string(),
 });
