3a2d9d00ddcd6194576cd2792e86c229079dfab7
The cool news guy <joewilliamdd31@gmail.com>
2024-12-25 06:34:48 +0100

    Add sample hours rows and style table's borders

--- src/components/ScheduleTable.tsx
+++ src/components/ScheduleTable.tsx
@@ -1,24 +1,52 @@
 import { days } from "@/lib/constants";
-import { Table, TableHead, TableHeader, TableRow } from "./ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
 export default function ScheduleTable() {
+  const hours = Array.from({ length: 9 }, (_, i) => i + 8);
+
   return (
-    <Table>
+    <Table className="border border-black dark:border-white">
       <TableHeader>
-        <TableRow>
+        <TableRow className="border-black dark:border-white">
           {/* Additional empty cell */}
           <TableHead></TableHead>
 
           {days.map((day) => (
             <TableHead
               key={day}
-              className="text-center text-black dark:text-white"
+              className="border-l border-black text-center text-black dark:border-white dark:text-white"
             >
               {day.toUpperCase()}
             </TableHead>
           ))}
         </TableRow>
       </TableHeader>
+
+      <TableBody>
+        {hours.map((hour) => (
+          <TableRow
+            key={hour}
+            className="h-[5rem] border-black dark:border-white"
+          >
+            <TableCell className="text-center font-bold">
+              {hour}:00 {hour < 12 ? "AM" : "PM"}
+            </TableCell>
+            {[...Array(7)].map((_, i) => (
+              <TableCell
+                key={i}
+                className="border-l border-black dark:border-white"
+              />
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   );
 }
