adc8e439d7797a0c48752ac349dc2001801fcd3a
The cool news guy <joewilliamdd31@gmail.com>
2024-12-26 20:43:10 +0100

    Add day, time props to Task class

--- src/lib/classes/Task.ts
+++ src/lib/classes/Task.ts
@@ -1,16 +1,23 @@
-import { priorities } from "@/lib/constants";
+import { days, priorities } from "@/lib/constants";
+import { Time } from "./Time";
 
 export class Task {
   name: string;
+  day: (typeof days)[number];
+  time: Time;
   priority: (typeof priorities)[number];
   description?: string;
 
   constructor(
     name: string,
+    day: typeof this.day,
+    time: Time,
     priority: typeof this.priority,
     description?: string,
   ) {
     this.name = name;
+    this.day = day;
+    this.time = time;
     this.priority = priority;
     this.description = description;
   }
