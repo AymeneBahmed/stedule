194f9ec2e14476467dc35f8cc8cb0941cd1f1413
The cool news guy <joewilliamdd31@gmail.com>
2024-12-26 08:11:41 +0100

    Add day prop to newTaskSchema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
+import { days } from "./constants";
 
 export const newTaskSchema = z.object({
   task: z.string().min(1, { message: "This field is required!" }),
+  day: z.enum(days),
   priority: z.enum(["unspecified", "low", "medium", "high"]),
   description: z.string(),
 });
