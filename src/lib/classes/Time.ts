f64f0287f6d375760668fd4268a6285c009d47f0
The cool news guy <joewilliamdd31@gmail.com>
2024-12-27 15:20:18 +0100

    Create fromString method for Time class

--- src/lib/classes/Time.ts
+++ src/lib/classes/Time.ts
@@ -8,4 +8,21 @@ export class Time {
     this.hour = hour;
     this.minute = minute;
   }
+
+  static fromString(time: string): Time | null {
+    const [hour, min] = time.split("").map(Number);
+
+    if (
+      !isNaN(hour) &&
+      !isNaN(min) &&
+      hour >= 0 &&
+      hour <= 23 &&
+      min >= 0 &&
+      min <= 59
+    ) {
+      return new Time(hour as Hour, min as Minute);
+    }
+
+    return null;
+  }
 }
