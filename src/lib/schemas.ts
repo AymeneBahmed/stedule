2ecbddb1ed8f799b05fe50f4d0db8cd8cea68c7b
The cool news guy <joewilliamdd31@gmail.com>
2024-12-27 14:59:43 +0100

    Add time prop to newTaskSchema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -4,6 +4,21 @@ import { days, priorities } from "./constants";
 export const newTaskSchema = z.object({
   task: z.string().min(1, { message: "This field is required!" }),
   day: z.enum(days),
+  time: z.string().refine(
+    (time) => {
+      const [hour, min] = time.split(":").map(Number);
+
+      return (
+        !isNaN(hour) &&
+        !isNaN(min) &&
+        hour >= 0 &&
+        hour <= 23 &&
+        min >= 0 &&
+        min <= 59
+      );
+    },
+    { message: "Invalid time!" },
+  ),
   priority: z.enum(priorities),
   description: z.string(),
 });
