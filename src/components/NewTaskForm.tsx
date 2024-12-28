4e4b19b1c960f57c7cc94b24965bedc05bf9934e
The cool news guy <joewilliamdd31@gmail.com>
2024-12-28 09:54:25 +0100

    Edit priority's default value in NewTaskForm

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -38,7 +38,7 @@ export default function NewTaskForm() {
         time == null
           ? undefined
           : `${time?.hour.toString().padStart(2, "0")}:${time?.minute.toString().padStart(2, "0")}`,
-      priority: "unspecified",
+      priority: "Unspecified",
       description: "",
     },
   });
