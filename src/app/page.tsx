f1e969f0ae0471cfb6e5bf7393467b21ea0cef72
The cool news guy <joewilliamdd31@gmail.com>
2024-12-28 09:23:02 +0100

    Fetch tasks and pass them to ScheduleTable in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import ScheduleTable from "@/components/ScheduleTable";
+import { Task } from "@/lib/classes/Task";
+import { Time } from "@/lib/classes/Time";
+import { getTasks } from "@/lib/db/task";
+import { Hour, Minute } from "@/lib/ts/types";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = ((await getTasks()) ?? []).map(
+    (task) =>
+      new Task(
+        task.name,
+        task.date.getDay(),
+        new Time(
+          task.date.getHours() as Hour,
+          task.date.getMinutes() as Minute,
+        ),
+        task.priority,
+      ),
+  );
+
   return (
     <div className="flex min-h-full items-center justify-center [&>div]:w-[80%]">
-      <ScheduleTable />
+      <ScheduleTable tasks={tasks} />
     </div>
   );
 }
