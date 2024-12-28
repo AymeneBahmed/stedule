84392269383133742fde922663d64aab158becc5
The cool news guy <joewilliamdd31@gmail.com>
2024-12-28 08:56:53 +0100

    Capitalize priorities in useTaskStore

--- src/lib/stores/tasks.ts
+++ src/lib/stores/tasks.ts
@@ -11,16 +11,16 @@ interface TasksStore {
 export const useTasksStore = create<TasksStore>((set) => ({
   // prettier-ignore
   tasks: [
-    new Task("Read a book", Day.Sunday, new Time(3, 10), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Tuesday, new Time(12, 40), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Saturday, new Time(16, 45), "medium", "Study as hard as you can"),
-    new Task("Read a book", Day.Monday, new Time(3, 8), "high", "Study as hard as you can"),
-    new Task("Read a book", Day.Friday, new Time(7, 20), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Wednesday, new Time(11, 30), "medium", "Study as hard as you can"),
-    new Task("Read a book", Day.Sunday, new Time(15, 40), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Wednesday, new Time(9, 45), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Thursday, new Time(12, 40), "low", "Study as hard as you can"),
-    new Task("Read a book", Day.Monday, new Time(12, 40), "low", "Study as hard as you can"),
+    new Task("Read a book", Day.Sunday, new Time(3, 10), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Tuesday, new Time(12, 40), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Saturday, new Time(16, 45), "Medium", "Study as hard as you can"),
+    new Task("Read a book", Day.Monday, new Time(3, 8), "High", "Study as hard as you can"),
+    new Task("Read a book", Day.Friday, new Time(7, 20), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Wednesday, new Time(11, 30), "Medium", "Study as hard as you can"),
+    new Task("Read a book", Day.Sunday, new Time(15, 40), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Wednesday, new Time(9, 45), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Thursday, new Time(12, 40), "Low", "Study as hard as you can"),
+    new Task("Read a book", Day.Monday, new Time(12, 40), "Low", "Study as hard as you can"),
   ],
   addTask(task) {
     set((state) => ({ tasks: [...state.tasks, task] }));
