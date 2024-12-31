274c2541758a3a74226f813bb97777d93b05f63d
The cool news guy <joewilliamdd31@gmail.com>
2024-12-31 09:49:39 +0100

    Edit tasks array in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import ScheduleTable from "@/components/ScheduleTable";
 import { Task } from "@/lib/classes/Task";
 import { getTasks } from "@/lib/db/task";
-import { Hour, Minute } from "@/lib/ts/types";
 
 export default async function Home() {
-  const tasks = ((await getTasks()) ?? []).map<Task>(
-    ({ id, name, date, priority, description }) => ({
-      id,
-      name,
-      priority,
-      day: date.getDay(),
-      time: {
-        hour: date.getHours() as Hour,
-        minute: date.getMinutes() as Minute,
-      },
-      description: description ?? undefined,
-    }),
-  );
+  const tasks = ((await getTasks()) ?? []).map<Task>((task) => ({
+    ...task,
+    description: task.description ?? undefined,
+  }));
 
   return (
     <div className="flex min-h-full items-center justify-center [&>div]:w-[80%]">
