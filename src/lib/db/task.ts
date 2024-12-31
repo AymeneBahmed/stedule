ed6cc61e19d1d3f30e5ddd7ec1ee4a3b909d1531
The cool news guy <joewilliamdd31@gmail.com>
2024-12-31 09:40:28 +0100

    Create createTask func in task.ts

--- src/lib/db/task.ts
+++ src/lib/db/task.ts
@@ -1,8 +1,25 @@
+import { Task } from "../classes/Task";
 import { prisma } from "../prisma";
+import { PrismaTaskModified } from "../ts/interfaces";
 
 export async function getTasks() {
   try {
     return await prisma.task.findMany();
+  } catch {
+    return null;
+  }
+}
+
+export async function createTask(task: Omit<Task, "id">) {
+  try {
+    return (await prisma.task.create({
+      data: {
+        name: task.name,
+        day: task.day,
+        time: task.time,
+        priority: task.priority,
+      },
+    })) as PrismaTaskModified;
   } catch {
     return null;
   }
