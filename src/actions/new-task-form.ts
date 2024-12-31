d335c54ca04e755ebb8e15dcaf6119b846c4c8ac
The cool news guy <joewilliamdd31@gmail.com>
2024-12-31 10:01:13 +0100

    Make addNewTask action functional

--- src/actions/new-task-form.ts
+++ src/actions/new-task-form.ts
@@ -1,13 +1,43 @@
 "use server";
 
+import { Time } from "@/lib/classes/Time";
+import { days } from "@/lib/constants";
+import { createTask } from "@/lib/db/task";
 import { newTaskSchema } from "@/lib/schemas";
+import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
 export async function addNewTask(
   _prevState: unknown,
   values: z.infer<typeof newTaskSchema>,
 ) {
+  const validated = newTaskSchema.safeParse(values);
+
+  if (!validated.success) {
+    return {
+      error: "Some fields are invalid!",
+    };
+  }
+
+  const { task, day, time, priority, description } = validated.data;
+
+  try {
+    await createTask({
+      name: task,
+      day: days.indexOf(day),
+      time: Time.fromString(time)!,
+      priority,
+      description,
+    });
+  } catch {
+    return {
+      error: "Something went wrong! Please try again.",
+    };
+  }
+
+  revalidatePath("/");
+
   return {
-    error: "Something went wrong! Please try again.",
+    success: "Added a task successfully!",
   };
 }
