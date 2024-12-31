a0f49cb422593b0929cc9fad61f39c9181a2b128
The cool news guy <joewilliamdd31@gmail.com>
2024-12-31 16:26:46 +0100

    Create private sortTimes method in useScheduleTimesStore

--- src/lib/stores/scheduleTimes.ts
+++ src/lib/stores/scheduleTimes.ts
@@ -9,21 +9,35 @@ interface ScheduleTimesStore {
   initializeWithDefaultTimes: () => void;
 }
 
-export const useScheduleTimesStore = create<ScheduleTimesStore>((set, get) => ({
-  times: [],
-  addTime(newTime) {
-    const timeExists = get().times.find((time) => Time.equals(time, newTime));
+export const useScheduleTimesStore = create<ScheduleTimesStore>((set, get) => {
+  // private method
+  function sortTimes() {
+    set((state) => ({
+      times: state.times.toSorted((a, b) =>
+        a.hour === b.hour ? a.minute - b.minute : a.hour - b.hour,
+      ),
+    }));
+  }
 
-    if (timeExists == null) {
-      set({ times: get().times.concat(newTime) });
-    }
-  },
-  removeTime(time) {
-    set({ times: get().times.filter((t) => !Time.equals(t, time)) });
-  },
-  initializeWithDefaultTimes() {
-    set({
-      times: Array.from({ length: 8 }, (_, i) => new Time((i + 8) as Hour, 0)),
-    });
-  },
-}));
+  return {
+    times: [],
+    addTime(newTime) {
+      const timeExists = get().times.find((time) => Time.equals(time, newTime));
+
+      if (timeExists == null) {
+        set({ times: get().times.concat(newTime) });
+      }
+    },
+    removeTime(time) {
+      set({ times: get().times.filter((t) => !Time.equals(t, time)) });
+    },
+    initializeWithDefaultTimes() {
+      set({
+        times: Array.from(
+          { length: 8 },
+          (_, i) => new Time((i + 8) as Hour, 0),
+        ),
+      });
+    },
+  };
+});
