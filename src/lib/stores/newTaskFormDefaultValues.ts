5155024aec394f05fb11602679579215cb01d1ee
noob <joewilliamdd31@gmail.com>
2025-02-09 21:08:22 +0100

    Add defaultTask prop to NewTaskFormDefaultValues

--- src/lib/stores/newTaskFormDefaultValues.ts
+++ src/lib/stores/newTaskFormDefaultValues.ts
@@ -1,22 +1,30 @@
 import { create } from "zustand";
 import { days } from "../constants";
 import { Time } from "../classes/Time";
+import { Task } from "../classes/Task";
 
 interface NewTaskFormDefaultValues {
   defaultDay: (typeof days)[number] | null;
   defaultTime: Time | null;
+  // Used if clicked on an occupied cell in the schedule table
+  defaultTask: Task | null;
   setDefaultDay: (day: (typeof days)[number] | null) => void;
   setDefaultTime: (time: Time | null) => void;
+  setDefaultTask: (task: Task | null) => void;
 }
 
 export const useNewTaskFormDefaultValuesStore =
   create<NewTaskFormDefaultValues>((set) => ({
     defaultDay: null,
     defaultTime: null,
+    defaultTask: null,
     setDefaultDay(day) {
       set({ defaultDay: day });
     },
     setDefaultTime(time) {
       set({ defaultTime: time });
+    },
+    setDefaultTask(task) {
+      set({ defaultTask: task });
     },
   }));
