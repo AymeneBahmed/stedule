f0c0e3eee4d25b4a8e3beccdde3d267efb4a4748
noob <joewilliamdd31@gmail.com>
2025-03-03 07:54:55 +0100

    Edit addNewTask() to update existing tasks

--- src/actions/new-task-form-actions.ts
+++ src/actions/new-task-form-actions.ts
@@ -3,6 +3,7 @@
 import { Time } from "@/lib/classes/Time";
 import { days } from "@/lib/constants";
 import { createTask } from "@/lib/db/task";
+import { prisma } from "@/lib/prisma";
 import { newTaskSchema } from "@/lib/schemas";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
@@ -22,13 +23,31 @@ export async function addNewTask(
   const { task, day, time, priority, description } = validated.data;
 
   try {
-    await createTask({
-      name: task,
-      day: days.indexOf(day),
-      time: Time.fromString(time)!,
-      priority,
-      description,
+    const existingTask = await prisma.task.findFirst({
+      where: {
+        day: days.indexOf(day),
+        time: Time.fromString(time)!,
+      },
     });
+
+    if (existingTask != null) {
+      await prisma.task.update({
+        where: { id: existingTask.id },
+        data: {
+          name: task,
+          priority,
+          description,
+        },
+      });
+    } else {
+      await createTask({
+        name: task,
+        day: days.indexOf(day),
+        time: Time.fromString(time)!,
+        priority,
+        description,
+      });
+    }
   } catch {
     return {
       error: "Something went wrong! Please try again.",
