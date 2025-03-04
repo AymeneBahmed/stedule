afe49dd6e10195541cbf4ffc1251e3760e2d19df
noob <joewilliamdd31@gmail.com>
2025-03-04 17:05:08 +0100

    Add the 8 default times to Times table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import ScheduleTable from "@/components/ScheduleTable";
 import { Task } from "@/lib/classes/Task";
 import { getTasks } from "@/lib/db/task";
+import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   const initialTasks = ((await getTasks()) ?? []).map<Task>((task) => ({
     ...task,
     description: task.description ?? undefined,
   }));
+
+  // The second condition make sure to not insert multiple default times if they already exist.
+  if (initialTasks.length === 0 && (await prisma.time.count()) === 0) {
+    await prisma.time.createMany({
+      data: Array.from({ length: 8 }, (_, i) => ({ hour: i + 8, minute: 0 })),
+    });
+  }
 
   return (
     <div className="flex min-h-full items-center justify-center [&>div]:w-[80%]">
