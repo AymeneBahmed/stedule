b8785bc3aab9b7768981097eb228d7ca4ff04f5b
noob <joewilliamdd31@gmail.com>
2025-03-05 05:02:10 +0100

    Create fetcher() util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// utility for SWR
+export async function fetcher(...args: Parameters<typeof fetch>) {
+  return await (await fetch(...args)).json();
+}
