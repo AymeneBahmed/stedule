3156d3fc0a51ac01018689a65b35c1a3fbe8fc84
noob <joewilliamdd31@gmail.com>
2025-03-05 05:06:40 +0100

    Remove actions/times.ts

--- src/actions/times.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { prisma } from "@/lib/prisma";
-import { Time } from "@prisma/client";
-
-export async function getTimes(): Promise<Time[]> {
-  return await prisma.time.findMany();
-}
