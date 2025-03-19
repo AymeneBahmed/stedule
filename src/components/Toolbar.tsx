6129e5986abc54c5a965dcf28c8b0d4afa4a3a03
noob <joewilliamdd31@gmail.com>
2025-03-19 05:32:14 +0100

    Add AddNewTaskButton to Toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,3 +1,4 @@
+import AddNewTaskButton from "./AddNewTaskButton";
 import AddNewTimeButton from "./AddNewTimeButton";
 import ThemeTogglerButton from "./ThemeTogglerButton";
 
@@ -5,6 +6,7 @@ export default function Toolbar() {
   return (
     <span className="bg-primary/50 space-x-1 rounded-full">
       <AddNewTimeButton />
+      <AddNewTaskButton />
       <ThemeTogglerButton className="rounded-full" />
     </span>
   );
