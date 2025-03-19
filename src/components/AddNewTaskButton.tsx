768429d3ff6a55da05043d2147c6e002ed564df3
noob <joewilliamdd31@gmail.com>
2025-03-19 05:31:52 +0100

    Create AddNewTaskButton

---
+++ src/components/AddNewTaskButton.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { ClipboardList } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useShouldOpenNewTaskFormStore } from "@/lib/stores/shouldOpenNewTaskFormStore";
+import NewTaskForm from "./NewTaskForm";
+
+export default function AddNewTaskButton(
+  props: React.ComponentProps<typeof Button>,
+) {
+  const { shouldOpenNewTaskForm, openNewTaskForm, closeNewTaskForm } =
+    useShouldOpenNewTaskFormStore();
+
+  return (
+    <Dialog
+      open={shouldOpenNewTaskForm}
+      onOpenChange={(open) => {
+        if (open) {
+          openNewTaskForm();
+        } else {
+          closeNewTaskForm();
+        }
+      }}
+    >
+      <DialogTrigger asChild onClick={openNewTaskForm}>
+        <Button size="icon" className="rounded-full" {...props}>
+          <ClipboardList className="scale-125" />
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add time</DialogTitle>
+          <DialogDescription className="sr-only">
+            Add new time
+          </DialogDescription>
+        </DialogHeader>
+
+        <NewTaskForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
