c2c081d2c8bfe2d04766bf8922ffcb7670095698
noob <joewilliamdd31@gmail.com>
2025-03-19 05:14:49 +0100

    Add ThemeTogglerButton to Toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,9 +1,11 @@
 import AddNewTimeButton from "./AddNewTimeButton";
+import ThemeTogglerButton from "./ThemeTogglerButton";
 
 export default function Toolbar() {
   return (
     <span className="bg-primary/50 space-x-1 rounded-full">
       <AddNewTimeButton />
+      <ThemeTogglerButton className="rounded-full" />
     </span>
   );
 }
