b86ece233b6b663c38307b5b50c093f308665469
noob <joewilliamdd31@gmail.com>
2025-03-20 09:09:10 +0100

    Use px-1.5 py-1 for Toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -4,7 +4,7 @@ import ThemeTogglerButton from "./ThemeTogglerButton";
 
 export default function Toolbar() {
   return (
-    <span className="bg-primary/15 flex space-x-1 rounded-full">
+    <span className="bg-primary/15 flex space-x-1 rounded-full px-1.5 py-1">
       <AddNewTimeButton />
       <AddNewTaskButton />
       <ThemeTogglerButton className="rounded-full" />
