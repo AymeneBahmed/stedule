d0372a8ff72b423276d08313805c6a1a22da3b55
noob <joewilliamdd31@gmail.com>
2025-03-20 07:05:38 +0100

    Remove useTimes hook

--- src/lib/hooks/useTimes.ts
+++
@@ -1,9 +0,0 @@
-import useSWR from "swr";
-import { fetcher } from "../utils";
-import { Time } from "@prisma/client";
-
-export function useTimes() {
-  const options = useSWR<Time | { error: string }>("/api/times", fetcher);
-
-  return options;
-}
