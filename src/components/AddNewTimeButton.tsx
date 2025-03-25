15026b7fffc56cb8b2f1e334aaa76fbcb8ae647e
noob <joewilliamdd31@gmail.com>
2025-03-25 06:04:31 +0100

    Remove open, onOpenChange props from AddNewTimeButton

--- src/components/AddNewTimeButton.tsx
+++ src/components/AddNewTimeButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "./ui/button";
 import { Clock } from "lucide-react";
 import {
@@ -11,26 +9,13 @@ import {
   DialogTrigger,
 } from "./ui/dialog";
 import NewTimeForm from "./NewTimeForm";
-import { useShouldOpenNewTimeFormStore } from "@/lib/stores/shouldOpenNewTimeFormStore";
 
 export default function AddNewTimeButton(
   props: React.ComponentProps<typeof Button>,
 ) {
-  const { shouldOpenNewTimeForm, openNewTimeForm, closeNewTimeForm } =
-    useShouldOpenNewTimeFormStore();
-
   return (
-    <Dialog
-      open={shouldOpenNewTimeForm}
-      onOpenChange={(open) => {
-        if (open) {
-          openNewTimeForm();
-        } else {
-          closeNewTimeForm();
-        }
-      }}
-    >
-      <DialogTrigger asChild onClick={openNewTimeForm}>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button size="icon" className="rounded-full" {...props}>
           <Clock className="scale-125" />
         </Button>
