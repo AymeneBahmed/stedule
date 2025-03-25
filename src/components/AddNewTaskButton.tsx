535df990f4809a06672ee3a3d8863132c71f774f
noob <joewilliamdd31@gmail.com>
2025-03-25 06:28:10 +0100

    Remove open, onOpenChange props from AddNewTaskButton

--- src/components/AddNewTaskButton.tsx
+++ src/components/AddNewTaskButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "./ui/button";
 import { ClipboardList } from "lucide-react";
 import {
@@ -10,27 +8,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
-import { useShouldOpenNewTaskFormStore } from "@/lib/stores/shouldOpenNewTaskFormStore";
 import NewTaskForm from "./NewTaskForm";
 
 export default function AddNewTaskButton(
   props: React.ComponentProps<typeof Button>,
 ) {
-  const { shouldOpenNewTaskForm, openNewTaskForm, closeNewTaskForm } =
-    useShouldOpenNewTaskFormStore();
-
   return (
-    <Dialog
-      open={shouldOpenNewTaskForm}
-      onOpenChange={(open) => {
-        if (open) {
-          openNewTaskForm();
-        } else {
-          closeNewTaskForm();
-        }
-      }}
-    >
-      <DialogTrigger asChild onClick={openNewTaskForm}>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button size="icon" className="rounded-full" {...props}>
           <ClipboardList className="scale-125" />
         </Button>
