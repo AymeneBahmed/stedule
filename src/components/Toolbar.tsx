6d955a7a5e352f154343ca6922b15e1c44fbad5d
noob <joewilliamdd31@gmail.com>
2025-07-10 14:36:16 +0100

    Use z-auto for Toolbar whenever any modal is open

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,22 +1,14 @@
-"use client";
-
-import { useShouldOpenNewTaskFormStore } from "@/lib/stores/shouldOpenNewTaskFormStore";
 import AddNewTaskButton from "./AddNewTaskButton";
 import AddNewTimeButton from "./AddNewTimeButton";
 import ClearScheduleButton from "./ClearScheduleButton";
 import PrintScheduleButton from "./PrintScheduleButton";
 import ThemeTogglerButton from "./ThemeTogglerButton";
-import { cn } from "@/lib/utils";
 
 export default function Toolbar() {
-  const { shouldOpenNewTaskForm } = useShouldOpenNewTaskFormStore();
-
   return (
     <span
-      className={cn(
-        "bg-primary/15 sticky top-10 flex space-x-2 rounded-full px-1.5 py-1 backdrop-blur-xl",
-        !shouldOpenNewTaskForm && "z-[9999999]",
-      )}
+      id="toolbar"
+      className="bg-primary/15 sticky top-10 z-[9999999] flex space-x-2 rounded-full px-1.5 py-1 backdrop-blur-xl"
     >
       <AddNewTimeButton />
       <AddNewTaskButton />
