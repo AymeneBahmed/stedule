f4f975b5c7e2569b630c37a909e17827bc7cf5c9
noob <joewilliamdd31@gmail.com>
2025-07-12 14:16:45 +0100

    Add guest mode support for clearing schedule

--- src/components/ClearScheduleButton.tsx
+++ src/components/ClearScheduleButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { RefreshCcw, TriangleAlert } from "lucide-react";
+import { RefreshCcw } from "lucide-react";
 import { Button } from "./ui/button";
 import { clearSchedule } from "@/actions/clear-schedule-actions";
 import {
@@ -14,17 +14,36 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "./ui/alert-dialog";
-import { useActionState, useEffect } from "react";
+import { startTransition, useActionState, useEffect } from "react";
 import { toast } from "sonner";
+import { useIsGuestMode } from "@/hooks/use-is-guest-mode";
+import { dexieDB } from "@/lib/db/dexieDB";
 
 function ClearScheduleButton({
   ...props
 }: React.ComponentProps<typeof Button>) {
   const [state, clearScheduleAction] = useActionState(clearSchedule, null);
+  const isGuestMode = useIsGuestMode();
+
+  function handleSubmit(e: React.MouseEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    startTransition(async () => {
+      if (isGuestMode) {
+        try {
+          await Promise.all([dexieDB.tasks.clear(), dexieDB.times.clear()]);
+        } catch {
+          toast.error("Couldn't clear the schedule! Please try again.");
+        }
+      } else {
+        clearScheduleAction();
+      }
+    });
+  }
 
   useEffect(() => {
     if (state?.error) {
-      toast(state?.error, { icon: <TriangleAlert /> });
+      toast.error(state.error);
     }
   }, [state?.error]);
 
@@ -47,7 +66,7 @@ function ClearScheduleButton({
 
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <form action={clearScheduleAction}>
+          <form onClick={handleSubmit}>
             <AlertDialogAction type="submit" asChild>
               <Button
                 variant="destructive"
