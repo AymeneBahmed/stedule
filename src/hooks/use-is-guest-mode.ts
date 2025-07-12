a0b9fdb456a0f5f2f16c2e81fce2ed521b30217c
noob <joewilliamdd31@gmail.com>
2025-07-12 07:25:16 +0100

    Create useIsGuestMode hook

---
+++ src/hooks/use-is-guest-mode.ts
@@ -0,0 +1,18 @@
+import { authClient } from "@/lib/auth/auth-client";
+import { useEffect, useState } from "react";
+
+export function useIsGuestMode() {
+  const [isGuestMode, setIsGuestMode] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const session = await authClient.getSession();
+
+      if (session.data == null) {
+        setIsGuestMode(true);
+      }
+    })();
+  }, []);
+
+  return isGuestMode;
+}
