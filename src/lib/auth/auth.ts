837d0527401f4547f081a7b2c94f353c520b1cdf
noob <joewilliamdd31@gmail.com>
2025-07-18 18:07:34 +0100

    Add Google authentication

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -10,7 +10,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   plugins: [
     emailOTP({
       async sendVerificationOTP({ email, otp, type }) {
