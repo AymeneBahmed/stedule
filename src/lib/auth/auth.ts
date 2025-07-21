4d71454bc0c82c25bab54c817aba1af8d65637c8
noob <joewilliamdd31@gmail.com>
2025-07-21 08:51:47 +0100

    Remove changeEmail property in auth.ts due to bugs

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -3,7 +3,6 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { emailOTP } from "better-auth/plugins";
 import { prisma } from "../prisma";
 import {
-  sendNewEmailVerificationMail,
   sendDeleteUserVerificationMail,
   sendEmailVerificationMail,
 } from "../mail";
@@ -55,12 +54,6 @@ export const auth = betterAuth({
       enabled: true,
       async sendDeleteAccountVerification({ user, url }) {
         await sendDeleteUserVerificationMail(user.email, url);
-      },
-    },
-    changeEmail: {
-      enabled: true,
-      async sendChangeEmailVerification({ newEmail, url }) {
-        await sendNewEmailVerificationMail(newEmail, url);
       },
     },
   },
