b3be949232a1c4eaf36b487bc8007dd8101f5ff2
noob <joewilliamdd31@gmail.com>
2025-07-21 05:49:08 +0100

    Create FormWarning comp

---
+++ src/components/FormWarning.tsx
@@ -0,0 +1,10 @@
+import { TriangleAlert } from "lucide-react";
+
+export default function FormWarning({ message }: { message: string }) {
+  return (
+    <div className="mt-8 flex items-center gap-3 rounded bg-orange-100 p-3 pl-5 text-sm text-orange-700 dark:bg-orange-400/20 dark:text-orange-200">
+      <TriangleAlert size={20} />
+      <div>{message}</div>
+    </div>
+  );
+}
