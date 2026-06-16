05018c18a3d3ae82c04dbbc6407467a23e2edd6e
Aymene <aymendd3131@gmail.com>
2026-06-16 17:52:53 +0100

    Add tooltips for Toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -5,6 +5,7 @@ import { AddNewTimeButton } from "./AddNewTimeButton";
 import { ClearScheduleButton } from "./ClearScheduleButton";
 import { PrintScheduleButton } from "./PrintScheduleButton";
 import { ThemeTogglerButton } from "./ThemeTogglerButton";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export function Toolbar() {
   return (
@@ -12,18 +13,50 @@ export function Toolbar() {
       id="toolbar"
       className="bg-primary/15 dark:bg-primary/30 sticky top-10 z-[1] flex space-x-2 rounded-full px-1.5 py-1 backdrop-blur-xl"
     >
-      <AddNewTimeButton aria-label="Add a new time" />
-      <AddNewTaskButton aria-label="Add a new task" />
-      <ThemeTogglerButton className="rounded-full" aria-label="Toggle theme" />
-      <PrintScheduleButton
-        className="rounded-full"
-        aria-label="Print schedule"
-      />
-      <ClearScheduleButton
-        variant="destructive"
-        className="rounded-full"
-        aria-label="Clear schedule"
-      />
+      <Tooltip>
+        <TooltipTrigger>
+          <AddNewTimeButton aria-label="Add a new time" />
+        </TooltipTrigger>
+        <TooltipContent>Add Time</TooltipContent>
+      </Tooltip>
+
+      <Tooltip>
+        <TooltipTrigger>
+          <AddNewTaskButton aria-label="Add a new task" />
+        </TooltipTrigger>
+        <TooltipContent>Add Task</TooltipContent>
+      </Tooltip>
+
+      <Tooltip>
+        <TooltipTrigger>
+          <ThemeTogglerButton
+            className="rounded-full"
+            aria-label="Toggle theme"
+          />
+        </TooltipTrigger>
+        <TooltipContent>Change Theme</TooltipContent>
+      </Tooltip>
+
+      <Tooltip>
+        <TooltipTrigger>
+          <PrintScheduleButton
+            className="rounded-full"
+            aria-label="Print schedule"
+          />
+        </TooltipTrigger>
+        <TooltipContent>Print Schedule</TooltipContent>
+      </Tooltip>
+
+      <Tooltip>
+        <TooltipTrigger>
+          <ClearScheduleButton
+            variant="destructive"
+            className="rounded-full"
+            aria-label="Clear schedule"
+          />
+        </TooltipTrigger>
+        <TooltipContent>Clear Schedule</TooltipContent>
+      </Tooltip>
     </span>
   );
 }
